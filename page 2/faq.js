2654341ca0c96633022bd8af24d6af942ca4f1e8
priyansh-design <mehtapriyansh2000@gmail.com>
2021-04-16 15:07:02 +0530

    files added

---
+++ page 2/faq.js
@@ -0,0 +1,135 @@
+var button1=document.getElementById("ques-button1");
+var drop1=document.getElementById("question-des1");
+var arrow1=document.getElementById("arrow1");
+
+
+var button2=document.getElementById("ques-button2");
+var drop2=document.getElementById("question-des2");
+var arrow2=document.getElementById("arrow2");
+
+var button3=document.getElementById("ques-button3");
+var drop3=document.getElementById("question-des3");
+var arrow3=document.getElementById("arrow3");
+
+var button4=document.getElementById("ques-button4");
+var drop4=document.getElementById("question-des4");
+var arrow4=document.getElementById("arrow4");
+
+var button5=document.getElementById("ques-button5");
+var drop5=document.getElementById("question-des5");
+var arrow5=document.getElementById("arrow5");
+
+var button6=document.getElementById("ques-button6");
+var drop6=document.getElementById("question-des6");
+var arrow6=document.getElementById("arrow6");
+
+var button7=document.getElementById("ques-button7");
+var drop7=document.getElementById("question-des7");
+var arrow7=document.getElementById("arrow7");
+
+var button8=document.getElementById("ques-button8");
+var drop8=document.getElementById("question-des8");
+var arrow8=document.getElementById("arrow8");
+
+var button9=document.getElementById("ques-button9");
+var drop9=document.getElementById("question-des9");
+var arrow9=document.getElementById("arrow9");
+button1.addEventListener("click",()=>{
+	drop1.classList.toggle("active");
+	if (arrow1.classList.contains("ion-chevron-right")) {
+		arrow1.classList.replace("ion-chevron-right","ion-chevron-up");
+	}else{
+		arrow1.classList.replace("ion-chevron-up","ion-chevron-right");
+	}
+});
+
+
+button2.addEventListener("click",()=>{
+	drop2.classList.toggle("active");
+	if (arrow2.classList.contains("ion-chevron-right")) {
+		arrow2.classList.replace("ion-chevron-right","ion-chevron-up");
+	}else{
+		arrow2.classList.replace("ion-chevron-up","ion-chevron-right");
+			}
+});
+
+
+
+button3.addEventListener("click",()=>{
+	drop3.classList.toggle("active");
+	if (arrow3.classList.contains("ion-chevron-right")) {
+		arrow3.classList.replace("ion-chevron-right","ion-chevron-up");
+	}else{
+		arrow3.classList.replace("ion-chevron-up","ion-chevron-right");
+			}
+});
+
+
+
+
+button4.addEventListener("click",()=>{
+	drop4.classList.toggle("active");
+	if (arrow4.classList.contains("ion-chevron-right")) {
+		arrow4.classList.replace("ion-chevron-right","ion-chevron-up");
+	}else{
+		arrow4.classList.replace("ion-chevron-up","ion-chevron-right");
+			}
+});
+
+
+
+
+button5.addEventListener("click",()=>{
+	drop5.classList.toggle("active");
+	if (arrow5.classList.contains("ion-chevron-right")) {
+		arrow5.classList.replace("ion-chevron-right","ion-chevron-up");
+	}else{
+		arrow5.classList.replace("ion-chevron-up","ion-chevron-right");
+			}
+});
+
+
+
+
+button6.addEventListener("click",()=>{
+	drop6.classList.toggle("active");
+	if (arrow6.classList.contains("ion-chevron-right")) {
+		arrow6.classList.replace("ion-chevron-right","ion-chevron-up");
+	}else{
+		arrow6.classList.replace("ion-chevron-up","ion-chevron-right");
+			}
+});
+
+
+
+button7.addEventListener("click",()=>{
+	drop7.classList.toggle("active");
+	if (arrow7.classList.contains("ion-chevron-right")) {
+		arrow7.classList.replace("ion-chevron-right","ion-chevron-up");
+	}else{
+		arrow7.classList.replace("ion-chevron-up","ion-chevron-right");
+			}
+});
+
+
+
+button8.addEventListener("click",()=>{
+	drop8.classList.toggle("active");
+	if (arrow8.classList.contains("ion-chevron-right")) {
+		arrow8.classList.replace("ion-chevron-right","ion-chevron-up");
+	}else{
+		arrow8.classList.replace("ion-chevron-up","ion-chevron-right");
+			}
+});
+
+
+
+
+button9.addEventListener("click",()=>{
+	drop9.classList.toggle("active");
+	if (arrow9.classList.contains("ion-chevron-right")) {
+		arrow9.classList.replace("ion-chevron-right","ion-chevron-up");
+	}else{
+		arrow9.classList.replace("ion-chevron-up","ion-chevron-right");
+			}
+});
